e8b39cc3bf7bcf01b1f0cb27fdc98da323c50d21
Anas Alam <anasalam.xyz@gmail.com>
2026-06-02 14:26:02 +0530

    fixes

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,8 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = pool;
